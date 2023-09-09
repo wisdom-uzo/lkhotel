1dd99fde82edd52e597c4ed11a0205369e628395
wisdom-uzo <wisdomuzodimna@gmail.com>
2023-09-09 10:16:57 +0100

    nav change

--- sections/Header.js
+++ sections/Header.js
@@ -1,9 +1,13 @@
-import { Button, Link } from '@mui/material'
-import React from 'react'
+import { Button } from '@mui/material'
+import React, { useState } from 'react'
+import Link from "next/link";
+import { HiMenu } from 'react-icons/hi'
 
 const Header = () => {
+
+  const [openMenu, setOPenMenu] = useState(true)
   return (
-    <nav className='py-3 border-2 border-black bg-black bg-opacity-5'>
+    <nav className='py-3 border-2  bg-gray-100 relative'>
      
       <div className="flex justify-around items-center">
 
@@ -12,7 +16,7 @@ const Header = () => {
           </div>
 
 
-          <ul className="flex space-x-6 list-none ">
+          <ul className="md:flex space-x-6 list-none hidden">
             <li className=' list-none'>
               <Link href="/rooms" className="decoration-transparent text-gray-600 hover:text-gray-800 font-bold font-serif text-lg">
                 Rooms
@@ -40,12 +44,48 @@ const Header = () => {
             </li>
           </ul>
 
-           <Button className='bg-yellow-400 '>BOOK  NOW</Button>
+           <Button className='bg-yellow-400 hidden md:block'>BOOK  NOW</Button>
+           <Button className='md:hidden text-[2rem] border-4 border-blue-400' 
+            onClick={() => setOPenMenu(!openMenu)}
+            ><HiMenu/></Button>
+
+   
       </div>
 
+      {openMenu &&
+      <ul className=" space-x-6 list-none md:hidden w-full top-16 mt-10 flex flex-col  items-center justify-center space-y-10">
+            <li className=''>
+              <Link href="/rooms" className=" text-gray-600 hover:text-gray-800 font-bold font-serif text-lg">
+                Rooms
+              </Link>
+            </li>
+            <li>
+              <Link href="/amenities" className="text-gray-600 hover:text-gray-800 font-bold font-serif text-lg ">
+                Amenities
+              </Link>
+            </li>
+            <li>
+              <Link href="/dining" className="text-gray-600 hover:text-gray-800 font-bold font-serif text-lg ">
+                Dining
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="text-gray-600 hover:text-gray-800 font-bold font-serif text-lg ">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link  href="/contact" className="text-gray-600 hover:text-gray-800 font-bold font-serif text-lg ">
+                Contact
+              </Link>
+            </li>
 
+            <li>
+            <Button className='bg-yellow-400 w-[100%] px-20'>Book A Room</Button>
+            </li>
+      </ul>
 
-
+      }
    
 
 
